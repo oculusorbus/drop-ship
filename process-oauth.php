97d4a9eb148ec2e9b092b38a63f8074dee33147b
oculusorbus <123515070+oculusorbus@users.noreply.github.com>
2023-01-24 17:34:34 -0600

    Update process-oauth.php

--- process-oauth.php
+++ process-oauth.php
@@ -1,5 +1,5 @@
 <?php
-include 'credentials/process_oauth_credentials.php';
+include '../credentials/process_oauth_credentials.php';
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
