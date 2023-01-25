2d705ff6114f1d960d9129613cf22a9c00738cf5
oculusorbus <jeremiahbarber@gmail.com>
2023-01-25 01:43:24 -0600

    Update db.php

--- db.php
+++ db.php
@@ -374,8 +374,9 @@ function getSoldiers($conn, $active=null, $filterby="", $all=false){
 	if(!$all){
 		$user_clause = "user_id = '".$_SESSION['userData']['user_id']."' ";
 	}
-	echo $sql;
+
 	$sql = "SELECT * FROM soldiers INNER JOIN users ON users.id = soldiers.user_id WHERE ".$user_clause.$active_clause.$filterby." AND project_id = '".$_SESSION['userData']['project_id']."' ORDER BY deceased, name";
+	echo $sql;
 	$result = $conn->query($sql);
 
 	if ($result->num_rows > 0) {
