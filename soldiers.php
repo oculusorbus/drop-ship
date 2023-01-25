e64fcf0d47467b44dc717738f3fd92dfeb848315
oculusorbus <jeremiahbarber@gmail.com>
2023-01-25 00:52:24 -0600

    Create soldiers.php

---
+++ soldiers.php
@@ -0,0 +1,29 @@
+<?php
+include 'db.php';
+include 'webhooks.php';
+include 'dropship.php';
+include 'header.php';
+?>
+		<div class="row" id="row3">
+			<div class="main">
+				<h2>Soldiers</h2>
+			    <div class="content">
+					<div id="soldiers" class="soldiers-panel">
+						<?php getSoldiers($conn); ?>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<!-- Footer -->
+		<div class="footer">
+		  <p>Drop Ship | Ohh Meed's Shorty Verse<br>Copyright © <span id="year"></span>
+		</div>
+	</div>
+  </div>
+</body>
+<?php
+// Close DB Connection
+$conn->close();
+?>
+</html>
