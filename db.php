2e2dd1a4d83244df0825c47cc9c304efee6b2ea1
oculusorbus <jeremiahbarber@gmail.com>
2023-01-27 01:40:08 -0600

    Update db.php

--- db.php
+++ db.php
@@ -223,7 +223,7 @@ function createBattle($conn, $wager) {
 }
 
 // Get current battles
-function getBattles($conn, $wager) {
+function getBattles($conn) {
 	$sql = "SELECT user_id, wager, username FROM battles INNER JOIN users ON users.id = battles.user_id WHERE battles.project_id = '".$_SESSION['userData']['project_id']."' AND active = '1' ORDER BY date_created DESC";
 	$result = $conn->query($sql);
 
