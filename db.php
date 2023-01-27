5c08a06d008ddcb70800763b5b594a13cb8ede1a
oculusorbus <jeremiahbarber@gmail.com>
2023-01-27 12:51:05 -0600

    Update db.php

--- db.php
+++ db.php
@@ -318,7 +318,7 @@ function getBattles($conn) {
 		echo "<li class='role'><strong>Battle:</strong>&nbsp;".$row["username"]."&nbsp;";
 		echo "<strong>Wager:</strong>&nbsp;".$row["wager"]."&nbsp;$".evaluateText("SCRIP")."&nbsp;";
 		// Show accept button if player didn't create the battle
-		if($row["user_id"] != $_SESSION['userData']['user_id']){
+		if($row["user_id"] != $_SESSION['userData']['user_id'] && checkBalance($conn) >= $row["wager"]){
 			echo '<form id="opponentForm" action="dashboard.php#barracks" method="post">
 			  <input type="hidden" id="opponent_id" name="opponent_id" value="'.$_SESSION['userData']['user_id'].'">
 			  <input type="hidden" id="battle_id" name="battle_id" value="'.$row["battle_id"].'">
