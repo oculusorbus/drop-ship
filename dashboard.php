400e65da5ec7b7b68f8ff86b9c85eb4ec7f4cc99
oculusorbus <jeremiahbarber@gmail.com>
2023-01-27 00:59:05 -0600

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -511,8 +511,8 @@ if($_SESSION['userData']['project_id'] == 1){
 			<div class="content" id="battles">
 				<h2>PvP Battles</h2>
 				<form id="wagerForm" action="dashboard.php" method="post">
-				  <label for="wager">Enter $SCRIP Wager:</label>
-				  <input id="wager" name="wager" value="">
+				  <label for="wager">Enter $SCRIP Wager:</label><br>
+				  <input id="wager" name="wager" value=""><br>
 				  <input class="small-button" type="submit" value="Create PvP Battle">
 				</form>
 			</div>
