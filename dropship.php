fa5d6dd2f0f8237fcf5b27b0bc43659f5d94351c
oculusorbus <jeremiahbarber@gmail.com>
2023-01-27 14:16:31 -0600

    Update dropship.php

--- dropship.php
+++ dropship.php
@@ -281,19 +281,24 @@ $prefix = "/".strtolower(str_replace(" ", "-", getProjectName($conn)))."/";
 // Kill player
 function kill($dropshipMarkup, $counter) {
 	global $prefix, $conn;
-	if(!isset($_SESSION["userData"]["creator_id"])){
+	if(!isset($_SESSION["userData"]["battle_id"])){
 		$dropshipMarkup.="<div class='round' id='".$counter."'><h3>Round ".$counter." - DEAD</h3>";
 	}else{
-		$opponent = getOpponentUsername($conn, $_SESSION["userData"]["battle_id"]);
-		$opponent_score = getOpponentScore($conn, $_SESSION["userData"]["battle_id"]);
-		$dropshipMarkup.="<div class='round' id='".$counter."'><h3>Round ".$counter." - DEAD ";
 		$battleMarkup = "";
-		if($counter > $opponent_score){
-			$battleMarkup = "(Defeated ".$opponent." score of ".$opponent_score.")</h3>";
-		}else if($counter < $opponent_score){
-			$battleMarkup = "(Lost to ".$opponent." score of ".$opponent_score.")</h3>";
-		}else if($counter < $opponent_score){
-			$battleMarkup = "(Tied with ".$opponent." score of ".$opponent_score.")</h3>";
+		$dropshipMarkup.="<div class='round' id='".$counter."'><h3>Round ".$counter." - DEAD ";
+		if(isset($_SESSION["userData"]["creator_id"])){
+			$opponent = getOpponentUsername($conn, $_SESSION["userData"]["battle_id"]);
+			$opponent_score = getOpponentScore($conn, $_SESSION["userData"]["battle_id"]);
+			if($counter > $opponent_score){
+				$battleMarkup = "(Defeated ".$opponent." score of ".$opponent_score.")</h3>";
+			}else if($counter < $opponent_score){
+				$battleMarkup = "(Lost to ".$opponent." score of ".$opponent_score.")</h3>";
+			}else if($counter < $opponent_score){
+				$battleMarkup = "(Tied with ".$opponent." score of ".$opponent_score.")</h3>";
+			}
+		}else if(isset($_SESSION["userData"]["opponent_id"])){
+			$creator = getCreatorUsername($conn, $_SESSION["userData"]["battle_id"]);
+			$battleMarkup = "(Score logged in battle with ".$creator.")</h3>";
 		}
 		$dropshipMarkup.=$battleMarkup;
 	}
