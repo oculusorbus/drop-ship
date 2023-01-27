5b4a6409f0c871a117e97406fc7a9572534e7b1c
oculusorbus <jeremiahbarber@gmail.com>
2023-01-27 02:10:39 -0600

    Update db.php

--- db.php
+++ db.php
@@ -232,11 +232,12 @@ function getBattles($conn) {
 	  while($row = $result->fetch_assoc()) {
 		echo "<ul class='roles'>";
 		echo "<li class='role'><strong>Battle:</strong>&nbsp;".$row["username"]."&nbsp;";
-		echo "<strong>Wager:</strong>&nbsp;".$row["wager"]."&nbsp;$".evaluateText("SCRIP")."</li>";		
+		echo "<strong>Wager:</strong>&nbsp;".$row["wager"]."&nbsp;$".evaluateText("SCRIP");		
 		echo '<form id="opponentForm" action="battles.php" method="post">
 		  <input type="hidden" id="battle_id" name="battle_id" value="'.$row["battle_id"].'">
 		  <input class="small-button" type="submit" value="Accept">
 		</form>';
+		echo "</li>";
 		echo "</ul>";
 	  }
 	}
