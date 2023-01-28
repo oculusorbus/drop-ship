885dc06a4a2011998fcf89853c309db63b7e3d9e
oculusorbus <jeremiahbarber@gmail.com>
2023-01-28 00:00:29 -0600

    Update header.php

--- header.php
+++ header.php
@@ -9,7 +9,7 @@
   <link href="dist/flexbox.css?var=<?php echo rand(0,999); ?>" rel="stylesheet">
 </head>
 <body>
-	<?php if (!isset($_POST['run'])){ ?>
+	<?php if (!isset($_POST['instant_replay'])){ ?>
 	<div id="loading">
 	  <img id="loading-image" src="images/loading.gif" alt="Loading..." />
 	</div>
