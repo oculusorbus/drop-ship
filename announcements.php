995fb8dd0882661405901cec676db12b4bb959e2
oculusorbus <jeremiahbarber@gmail.com>
2023-01-29 12:41:31 -0600

    Update announcements.php

--- announcements.php
+++ announcements.php
@@ -14,16 +14,18 @@ $wager = $_GET["wager"];
 $opponent = $_GET["opponent"];
 $creator = $_GET["creator"];
 $opponent_score = $_GET["opponent_score"];
+$battle = $_GET["battle"];
+$scrip = $_GET["scrip"];
 
 //sleep($score*2);
 
-announceBattleResults($type, $user_id, $battle_id, $name, $score, $prefix, $avatar_url, $project_id, $wager, $opponent, $creator, $opponent_score);
+announceBattleResults($type, $user_id, $battle_id, $name, $score, $prefix, $avatar_url, $project_id, $wager, $opponent, $creator, $opponent_score, $battle, $srip);
 
 // Announce battle results
-function announceBattleResults($type, $user_id, $battle_id, $name, $score, $prefix, $avatar_url, $project_id, $wager, $opponent, $creator, $opponent_score){
+function announceBattleResults($type, $user_id, $battle_id, $name, $score, $prefix, $avatar_url, $project_id, $wager, $opponent, $creator, $opponent_score, $battle, $srip){
 	//global $conn;
 	
-	$title = "PvP ".evaluateText("Battle").": Dead on Round ".$score;
+	$title = "PvP ".$battle.": Dead on Round ".$score;
 	// Disabling inventory list for battles because it's tied to results and game id
 	//ob_start(); // Start output buffering
 	//checkPlayerItems($conn);
@@ -34,18 +36,18 @@ function announceBattleResults($type, $user_id, $battle_id, $name, $score, $pref
 	// "\n".evaluateText($list)
 	
 	if($type == "opponent"){
-		$description = $name." died during Round ".$score." in battle with ".$creator." for ".$wager." $".evaluateText("SCRIP").". It is now ".$creator."'s turn to defend.";
+		$description = $name." died during Round ".$score." in battle with ".$creator." for ".$wager." $".$scrip.". It is now ".$creator."'s turn to defend.";
 	}else if($type == "creator"){
 		$battle_markup = "";
 		if($score > $opponent_score){
 			$title = "WINNER - ".$title;
-			$battle_markup = " and won ".$wager." $".evaluateText("SCRIP")." against score of ".$opponent_score;
+			$battle_markup = " and won ".$wager." $".$scrip." against score of ".$opponent_score;
 		}else if($score < $opponent_score){
 			$title = "LOSER - ".$title;
-			$battle_markup = " and lost ".$wager." $".evaluateText("SCRIP")." against score of ".$opponent_score;
+			$battle_markup = " and lost ".$wager." $".$scrip." against score of ".$opponent_score;
 		}else if($score == $opponent_score){
 			$title = "TIE - ".$title;
-			$battle_markup = " and kept ".$wager." $".evaluateText("SCRIP")." by tying with score of ".$opponent_score;
+			$battle_markup = " and kept ".$wager." $".$scrip." by tying with score of ".$opponent_score;
 		}
 		$description = $name." died during Round ".$score.$battle_markup." by ".$opponent;
 	}
