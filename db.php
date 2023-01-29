ec99ede0c1cb55f13ba5f470e25c9c9d38b9a560
oculusorbus <jeremiahbarber@gmail.com>
2023-01-29 00:44:59 -0600

    Update db.php

--- db.php
+++ db.php
@@ -1902,7 +1902,8 @@ function transactionHistory($conn) {
 		echo "<table cellspacing='0' id='transactions'><tr><th>Date</th><th>Time</th><th align='center'>Type</th><th align='center'>\$".evaluateText("SCRIP")."</th><th align='center'>Icon</th><th>Description</th><th align='center'>Game</th><th align='center'>Score</th></tr>";
 	  	$xp = "<img class='icon' src='icons/xp.png'/>";
 		$scrip = "<img class='icon' src='icons/scrip.png'/>";
-		$weapons = "<img class='icon' src='icons/dropship.png'/>";
+		$win = "<img class='icon' src='icons/trophy.png'/>";
+		$loss = "<img class='icon' src='icons/skull.png'/>";
 		while($row = $result->fetch_assoc()) {
 			$type = "<img class='icon' src='icons/".$row["type"].".png'/>";
 			if($row["amount"] == "0" && isset($row["game_id"])){
@@ -1919,7 +1920,7 @@ function transactionHistory($conn) {
 			if ($row["type"] == "credit"){
 	    		echo "<td>".$date."</td><td>".$time."</td><td align='center'>".$type."</td><td align='center'>".$row["amount"]."</td><td align='center'>";
 				if($row["battle_id"] != 0){
-					echo $weapons;
+					echo $win;
 					echo "</td><td>";
 					if($row["creator_id"] == $_SESSION['userData']['user_id']){
 						$score = $row["user_score"]."-".$row["opponent_score"];
@@ -1946,7 +1947,7 @@ function transactionHistory($conn) {
 						$score = $row["opponent_score"]."-".$row["user_score"];
 						$username = getCreatorUsername($conn, $row["battle_id"]);
 					}
-					echo "<td align='center'>".$weapons."</td><td>".$username."</td><td align='center'>".$row["battle_id"]."</td><td align='center'>".$score."</td>";
+					echo "<td align='center'>".$loss."</td><td>".$username."</td><td align='center'>".$row["battle_id"]."</td><td align='center'>".$score."</td>";
 				}else{
 					echo "<td align='center'><img class='icon' src='icons/".evaluateText(strtolower(str_replace(" ", "-", $row["name"]))).".png'/></td><td>".evaluateText($row["name"])."</td><td>&nbsp;</td><td>&nbsp;</td>";
 				}
