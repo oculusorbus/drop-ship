511dd2648b074cfa52ff48b063dbbc6814bf52d3
oculusorbus <jeremiahbarber@gmail.com>
2023-01-29 04:01:01 -0600

    Update announcements.php

--- announcements.php
+++ announcements.php
@@ -20,41 +20,6 @@ announceBattleResults($type, $user_id, $battle_id, $name, $score, $prefix, $avat
 
 // Announce battle results
 function announceBattleResults($type, $user_id, $battle_id, $name, $score, $prefix, $avatar_url){
-	/*
-	global $conn;
-	$wager = getWager($conn, $battle_id);
-	$opponent = getOpponentUsername($conn, $battle_id);
-	$creator = getCreatorUsername($conn, $battle_id);
-	
-	$title = "PvP ".evaluateText("Battle").": Dead on Round ".$score;
-	// Disabling inventory list for battles because it's tied to results and game id
-	//ob_start(); // Start output buffering
-	//checkPlayerItems($conn);
-	//$list = ob_get_contents(); // Store buffer in variable
-	//ob_end_clean(); // End buffering and clean up
-	
-	// Append this to description variable if you get it working
-	// "\n".evaluateText($list)
-	
-	if($type == "opponent"){
-		$description = $name." died during Round ".$score." in battle with ".$creator." for ".$wager." $".evaluateText("SCRIP").". It is now ".$creator."'s turn to defend.";
-	}else if($type == "creator"){
-		$opponent_score = getOpponentScore($conn, $battle_id);
-		$battle_markup = "";
-		if($score > $opponent_score){
-			$title = "WINNER - ".$title;
-			$battle_markup = " and won ".$wager." $".evaluateText("SCRIP")." against score of ".$opponent_score;
-		}else if($score < $opponent_score){
-			$title = "LOSER - ".$title;
-			$battle_markup = " and lost ".$wager." $".evaluateText("SCRIP")." against score of ".$opponent_score;
-		}else if($score == $opponent_score){
-			$title = "TIE - ".$title;
-			$battle_markup = " and kept ".$wager." $".evaluateText("SCRIP")." by tying with score of ".$opponent_score;
-		}
-		$description = $name." died during Round ".$score.$battle_markup." by ".$opponent;
-	}
-	$imageurl = $avatar_url;
-	discordmsg($title, $description, $imageurl, "https://madballs.net/drop-ship/battles.php");*/
-}
 
+}
 ?>
