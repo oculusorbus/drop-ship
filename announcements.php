39d4d0327ad2ad167fbe76df79e02c013b932989
oculusorbus <jeremiahbarber@gmail.com>
2023-01-29 03:54:23 -0600

    Update announcements.php

--- announcements.php
+++ announcements.php
@@ -5,7 +5,17 @@ $prefix = $_GET["prefix"];
 include 'webhooks.php';
 //include 'dropship.php';
 //sleep(60);
-echo "here";
+
+$type = $_GET["type"];
+$user_id = $_GET["user_id"];
+$battle_id = $_GET["battle_id"];
+$name = $_GET["name"];
+$score = $_GET["score"];
+$prefix = $_GET["prefix"];
+$avatar_url = $_GET["avatar_url"];
+
+announceBattleResults($type, $user_id, $battle_id, $name, $score, $prefix, $avatar_url);
+
 // Announce battle results
 function announceBattleResults($type, $user_id, $battle_id, $name, $score, $prefix, $avatar_url){
 	global $conn;
@@ -44,5 +54,4 @@ function announceBattleResults($type, $user_id, $battle_id, $name, $score, $pref
 	discordmsg($title, $description, $imageurl, "https://madballs.net/drop-ship/battles.php");
 }
 
-announceBattleResults($_GET["type"], $_GET["user_id"], $_GET["battle_id"], $_GET["name"], $_GET["score"], $_GET["prefix"], $_GET["avatar_url"]);
 ?>
