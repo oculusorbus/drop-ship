96b2e4dbbd8db681b83ddeb87b656cf8ca9a1794
oculusorbus <jeremiahbarber@gmail.com>
2023-01-28 23:41:10 -0600

    Update db.php

--- db.php
+++ db.php
@@ -1917,10 +1917,12 @@ function transactionHistory($conn) {
 				if($row["battle_id"] != 0){
 					if($row["creator_id"] == $_SESSION['userData']['user_id']){
 						$score = $row["user_score"]."/".$row["opponent_score"];
+						$username = getOpponentUsername($conn, $row["battle_id"]);
 					}else{
 						$score = $row["opponent_score"]."/".$row["user_score"];
+						$username = getCreatorUsername($conn, $row["battle_id"]);
 					}
-					echo "Won Battle";
+					echo "Defeated ".$username;
 					echo "</td><td align='center'>".$row["battle_id"]."</td><td align='center'>".$score."</td>";
 				}else{
 					echo ($row["amount"] == "0") ? "Winner" : "Reward";
@@ -1930,10 +1932,12 @@ function transactionHistory($conn) {
 				if($row["battle_id"] != 0){
 					if($row["creator_id"] == $_SESSION['userData']['user_id']){
 						$score = $row["user_score"]."/".$row["opponent_score"];
+						$username = getOpponentUsername($conn, $row["battle_id"]);
 					}else{
 						$score = $row["opponent_score"]."/".$row["user_score"];
+						$username = getCreatorUsername($conn, $row["battle_id"]);
 					}
-					echo "<td>".$date."</td><td>".$time."</td><td align='center'>".ucfirst($row["type"])."</td><td align='center'>".$row["amount"]."</td><td align='center'>".$scrip."</td><td>Lost Battle</td><td align='center'>".$row["battle_id"]."</td><td align='center'>".$score."</td>";
+					echo "<td>".$date."</td><td>".$time."</td><td align='center'>".ucfirst($row["type"])."</td><td align='center'>".$row["amount"]."</td><td align='center'>".$scrip."</td><td>Lost to ".$username."</td><td align='center'>".$row["battle_id"]."</td><td align='center'>".$score."</td>";
 				}else{
 					echo "<td>".$date."</td><td>".$time."</td><td align='center'>".ucfirst($row["type"])."</td><td align='center'>".$row["amount"]."</td><td align='center'><img class='icon' src='icons/".evaluateText(strtolower(str_replace(" ", "-", $row["name"]))).".png'/></td><td>".evaluateText($row["name"])."</td><td>&nbsp;</td><td>&nbsp;</td>";
 				}
