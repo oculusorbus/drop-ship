8768c8d50272cb0c94f424725a3026591af0d19f
oculusorbus <jeremiahbarber@gmail.com>
2023-01-29 03:57:56 -0600

    Update announcements.php

--- announcements.php
+++ announcements.php
@@ -7,12 +7,7 @@ include 'webhooks.php';
 //sleep(60);
 
 $type = $_GET["type"];
-$user_id = $_GET["user_id"];
-$battle_id = $_GET["battle_id"];
-$name = $_GET["name"];
-$score = $_GET["score"];
-$prefix = $_GET["prefix"];
-$avatar_url = $_GET["avatar_url"];
+
 
 
 //announceBattleResults($type, $user_id, $battle_id, $name, $score, $prefix, $avatar_url);
