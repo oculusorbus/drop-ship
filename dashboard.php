abd4ddc0b46131cf5c9d0a1344d98c6b76f55434
oculusorbus <jeremiahbarber@gmail.com>
2023-02-11 11:59:49 -0600

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -487,13 +487,12 @@ if($_SESSION['userData']['project_id'] == 1){
 					<?php
 						
 						$checked = "checked";
-						/*
 						echo $_SESSION['userData']['audio'];
 						if($_SESSION['userData']['audio']){
 							$checked = "checked";
 						}else{
 							$checked = "";
-						}*/
+						}
 					?>
 					<li class="role"><img class="icon" id="audio-icon" src="icons/audio-on.png"/>Audio
 						&nbsp;
