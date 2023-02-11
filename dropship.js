4605cbc4ae480e2bc25ec60a2a489a6bae7a823f
oculusorbus <jeremiahbarber@gmail.com>
2023-02-11 12:08:38 -0600

    Update dropship.js

--- dropship.js
+++ dropship.js
@@ -22,8 +22,7 @@ function toggleAudio(status){
 		audioIcon.src = "icons/audio-off.png";
 		status = "false";
 	}
-	alert(audio1);
-	if(audio1 != null && audio2 != null){
+	if(typeof audio1 === 'object'){
 		if(status){
 			audio1.muted = false;
 			audio2.muted = false;
