d5cc7b4c93ee9be26300f94dac9807d4d8b51ffa
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2023-09-24 17:05:28 -0500

    Update discoin.php

--- discoin.php
+++ discoin.php
@@ -13,8 +13,12 @@ if(!isset($_SESSION['userData']['transaction'])){
 ?>
 		<div class="row" id="row4">
 			<div class="main">
-				<h2>Buy Temporary VIP Pass with DISCOIN</h2>
+				<h2>Buy Temporary VIP Access with DISCOIN</h2>
 				<div class="content">
+					<p>
+					If you don't hold a VIP token, you can purchase temporary VIP access with DISCOIN. You will receive temporary VIP roles in Discord and temporary access to the Oculus Lounge game.<br>
+					Be warned that the Oculus Lounge bouncers may kick you out in a few minutes or a few hours for sneaking into the VIP lounge. Even if you're kicked out quickly, your session for the game will last as long as you are logged in.
+					</p>
 					<p>
 					Please send 1.<?php echo $_SESSION['userData']['transaction']; ?> ADA and 1,000 DISCOIN in a single transaction to the following address:
 					</p>
@@ -26,7 +30,7 @@ if(!isset($_SESSION['userData']['transaction'])){
 					</p>
 					<button type="button" onclick="checkTransaction();">Verify Transaction</button>
 					<p>
-					Upon successful verification of the transaction, you will receive a confirmation message that you've received a temporary VIP role in Oculus Lounge discord.
+					Upon successful verification of the transaction, you will receive a confirmation message.
 					</p>
 					<p>
 					If several minutes pass with no confirmation, feel free to refresh this webpage and verify again. You do not need to send another transaction to verify after refreshing.
