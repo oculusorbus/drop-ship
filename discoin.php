d9eca73df1a488f6d2d83b7e6642272050b2ce78
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2023-09-24 03:24:01 -0500

    Update discoin.php

--- discoin.php
+++ discoin.php
@@ -8,63 +8,6 @@ if(!isset($_SESSION['userData']['transaction'])){
 	$six_digit_random_number = random_int(200000, 999999);
 	$_SESSION['userData']['transaction'] = $six_digit_random_number;
 }
-
-$discoin_policy_id = "5612bee388219c1b76fd527ed0fa5aa1d28652838bcab4ee4ee63197";
-
-
-	$ch = curl_init("https://api.koios.rest/api/v0/address_txs");
-	curl_setopt( $ch, CURLOPT_HTTPHEADER, array('Content-type: application/json'));
-	curl_setopt( $ch, CURLOPT_POST, 1);
-	curl_setopt( $ch, CURLOPT_POSTFIELDS, '{"_addresses":["addr1qykk9ue0wmnky9mh453ln84tf472036wqmhj46a45m6a8xqpqyck03v2n0nhz94r39gymw6q9xa0d8pg6daf3rsz7y3qdy8m9t"],"_after_block_height":6238675}');
-	curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 1);
-	curl_setopt( $ch, CURLOPT_HEADER, 0);
-	curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
-
-	$response = curl_exec( $ch );
-	// If you need to debug, or find out why you can't send message uncomment line below, and execute script.
-	$response = json_decode($response);
-	//print_r($response[0]->asset_list);
-	//exit;
-	curl_close( $ch );
-
-	$transaction_hash = $response[0]->tx_hash;
-
-	$ch = curl_init("https://api.koios.rest/api/v0/tx_info");
-	curl_setopt( $ch, CURLOPT_HTTPHEADER, array('Content-type: application/json'));
-	curl_setopt( $ch, CURLOPT_POST, 1);
-	curl_setopt( $ch, CURLOPT_POSTFIELDS, '{"_tx_hashes":["'.$transaction_hash.'"]}');
-	curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 1);
-	curl_setopt( $ch, CURLOPT_HEADER, 0);
-	curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
-
-	$response = curl_exec( $ch );
-	// If you need to debug, or find out why you can't send message uncomment line below, and execute script.
-	$response = json_decode($response);
-	//print_r($response[0]->asset_list);
-	//exit;
-	curl_close( $ch );
-	
-	$count = count($response[0]->outputs)-1;
-	$ada = $response[0]->outputs[$count]->value;
-	$quantity = $response[0]->outputs[$count]->asset_list[0]->quantity;
-	$policy_id = $response[0]->outputs[$count]->asset_list[0]->policy_id;
-
-	echo "ADA: ".$ada."<br>";
-	echo "Qty: ".$quantity."<br>";
-	echo "PID: ".$policy_id."<br>";
-	
-	//print_r($response);
-
-	$_SESSION['userData']['transaction'] = "187084";
-	if(str_contains($ada, $_SESSION['userData']['transaction'])){
-		if($policy_id == $discoin_policy_id){
-			if($quantity == 100000000000){ 
-				$flag = true;
-				echo "true";
-			}
-		}
-	}
-
 ?>
 		<div class="row" id="row4">
 			<div class="main">
@@ -90,6 +33,6 @@ $conn->close();
 ?>
 <script type="text/javascript" src="dropship.js"></script>
 <script type="text/javascript">
-	//checkTransaction();
+	checkTransaction();
 </script>
 </html>
