b2cca6c230fd4e4c84f259dac52a78da3c727197
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2023-09-24 15:05:49 -0500

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 </head>
 <body>
     <div class="flex items-center justify-center h-screen bg-discord-gray">
-        <a href="https://discord.com/api/oauth2/authorize?client_id=983993436694794261&redirect_uri=https%3A%2F%2Fmadballs.net%2Fdrop-ship%2Fprocess-oauth.php&response_type=code&scope=identify%20guilds%20guilds.members.read" class="bg-discord-blue px-5 py-3 rounded-md hover:bg-gray-600 transition">
+        <a href="https://discord.com/api/oauth2/authorize?client_id=983993436694794261&permissions=268437504&redirect_uri=https%3A%2F%2Fmadballs.net%2Fdrop-ship%2Fprocess-oauth.php&response_type=code&scope=identify%20guilds%20guilds.members.read" class="bg-discord-blue px-5 py-3 rounded-md hover:bg-gray-600 transition">
             <span class="text-white font-semibold text-xl"><i class="fa-brands fa-discord mr-2"></i> Login with Discord</span>
         </a>
     </div>
