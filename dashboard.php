aa783df6ec1aa45f50fda4044de4d0594af10ae1
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2024-04-24 22:28:15 -0500

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -134,7 +134,7 @@ if((isset($_SESSION['userData']['address']) && $address_changed == "true") || $p
 		$ch = curl_init("https://api.koios.rest/api/v1/aaccount_utxos?select=asset_list&asset_list=not.is.null");
 		curl_setopt( $ch, CURLOPT_HTTPHEADER, array('Content-type: application/json', 'accept: application/json', 'authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhZGRyIjoic3Rha2UxdXlxc3p2dDhjazlmaGVtM3o2M2NqNXpkaGRxem53aGtuczVkeDc1YzNjcDB6Z3MwODR1OGoiLCJleHAiOjE3MzQ3MDc5OTUsInRpZXIiOjEsInByb2pJRCI6InNrdWxsaWFuY2UifQ.eYZU74nwkN_qD8uK0UIv9VLveZLXMfJHznvzPWmnrq0'));
 		curl_setopt( $ch, CURLOPT_POST, 1);
-		curl_setopt( $ch, CURLOPT_POSTFIELDS, '{"_stake_addresses":["'.$_SESSION['userData']['address'].'"]}');
+		curl_setopt( $ch, CURLOPT_POSTFIELDS, '{"_stake_addresses":["'.$_SESSION['userData']['address'].'"],"_extended":true}');
 		curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 1);
 		curl_setopt( $ch, CURLOPT_HEADER, 0);
 		curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
