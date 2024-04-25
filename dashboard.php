90b4667138e013b17ffd1c645bc2912a01f34de3
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2024-04-24 21:02:17 -0500

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -152,9 +152,9 @@ if((isset($_SESSION['userData']['address']) && $address_changed == "true") || $p
 	    if(isset($response[0])){
 			$asset_names = array();
 			$counter = 0;
-			foreach($response[0]->asset_list AS $index => $token){
+			foreach($response[0] AS $index => $token){
 				if($token->policy_id == $policy_id){
-					$tokench = curl_init("https://api.koios.rest/api/v0/asset_info?_asset_policy=".$token->policy_id."&_asset_name=".$token->asset_name);
+					$tokench = curl_init("https://api.koios.rest/api/v1/asset_info?_asset_policy=".$token->policy_id."&_asset_name=".$token->asset_name);
 					curl_setopt( $tokench, CURLOPT_RETURNTRANSFER, 1);
 					$tokenresponse = curl_exec( $tokench );
 					$tokenresponse = json_decode($tokenresponse);
