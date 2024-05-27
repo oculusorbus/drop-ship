951a8cad9f1aabd3a1d80161d676b06f9bbc4f1f
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2024-05-27 02:17:01 -0500

    Create automate.php

---
+++ automate.php
@@ -0,0 +1,29 @@
+<?php
+include 'db.php';
+include 'dropship.php';
+include 'webhooks.php';
+
+if(isset($argv)){
+	parse_str(implode('&', array_slice($argv, 1)), $_GET);
+}
+// Validate cron job execution
+if(isset($_GET['automate'])){	
+	// Automatically end current game
+	$title = "Game Over";
+	ob_start(); // Start output buffering
+	checkLeaderboard($conn, "true");
+	$list = ob_get_contents(); // Store buffer in variable
+	ob_end_clean(); // End buffering and clean up
+	$description = $list;
+	$imageurl = "https://www.madballs.net/".$prefix."/images/dropship.jpg";
+	discordmsg($title, evaluateText($description), $imageurl, $_SESSION['userData']['project_id']);
+	deactivateGame($conn);
+
+	// Automatically create new game
+	createGame($conn, $_POST['name'], $_POST['prizes']);
+	$title = "New Game 1K, 500, 250 DREAD";
+	$description = "A new game of ".getProjectName($conn)." has been created.";
+	$imageurl = "https://www.madballs.net/".$prefix."/images/dropship.jpg";
+	discordmsg($title, $description, $imageurl, $_SESSION['userData']['project_id']);
+}
+?>
