8985f5369810959967d04fcedff5d5e3d0975a53
Oculus Orbus <123515070+oculusorbus@users.noreply.github.com>
2024-06-26 00:48:24 -0500

    Update db.php

--- db.php
+++ db.php
@@ -1930,7 +1930,7 @@ function logBalances($conn) {
 				logCredit($conn, $row["user_id"], $row["id"], 0);
 				
 				// CURL request to update Skulliance DB with DREAD allocations for winners, only execute on live Drop Ship
-				if(($_SESSION['userData']['project_id'] == 1 || $_SESSION['userData']['project_id'] == 4)){
+				if($_SESSION['userData']['project_id'] == 1 || $_SESSION['userData']['project_id'] == 4){
 					// set post fields
 					$post = [
 					    'discord_id' => $row["discord_id"],
@@ -1939,9 +1939,12 @@ function logBalances($conn) {
 					];
 					if(!str_contains($_SERVER['REQUEST_URI'], "test")){
 						$ch = curl_init('http://www.skulliance.io/staking/db.php');
+						echo 'http://www.skulliance.io/staking/db.php';
 					}else{
 						$ch = curl_init('http://www.skulliance.io/testing/db.php');
+						echo 'http://www.skulliance.io/testing/db.php';
 					}
+					exit;
 					curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 					curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
 
